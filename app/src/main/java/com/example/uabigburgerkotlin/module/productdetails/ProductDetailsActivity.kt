0aa20d3383761240ce2b706e06c71cd7665ed365
Wessim <wessim.trimeche@gmail.com>
2019-04-05 12:54:09 +0100

    ProductDetails code cleanup

--- app/src/main/java/com/example/uabigburgerkotlin/module/productdetails/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/uabigburgerkotlin/module/productdetails/ProductDetailsActivity.kt
@@ -31,83 +31,85 @@ class ProductDetailsActivity : AppCompatActivity(), ProductDetailsView {
     private lateinit var presenter: ProductDetailsPresenter
     private var isAddedToBaset: Boolean = false
 
+    companion object {
+        const val catalogProductKey = "CATALOG_PRODUCT"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_details)
-
-        UABigBurgerKotlinApp.uaBigBurgerAppComponent.inject(this)
-
-        bottomSheetBehavior = BottomSheetBehavior.from(popup_bottom_sheet)
         setSupportActionBar(product_details_toolbar)
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        injectDependencies()
+        initBottomSheet()
         presenter = ProductDetailsPresenter(this)
         if (intent.extras != null) {
-            catalogProduct =
-                Gson().fromJson(intent?.getExtras()?.getString(catalogProductKey), CatalogProductModel::class.java)
-            title = catalogProduct.title
-            isAddedToBaset = preferences.getBoolean(catalogProduct.ref, false)
-            product_details_price.text = String.format(getResources().getString(R.string.format), catalogProduct.price)
-            product_details_description.text = catalogProduct.description
-            Glide.with(applicationContext)
-                .load(catalogProduct.thumbnail)
-                .apply(
-                    RequestOptions()
-                        .placeholder(R.drawable.no_image)
-                        .error(R.drawable.no_image)
-                )
-                .into(product_details_thumbnail)
-            if (isAddedToBaset) {
-                product_details_button.setText(R.string.remove_from_basket)
-                popup_title.setText(R.string.confirm_delete_popup)
-            } else {
-                product_details_button.setText(R.string.add_to_basket)
-                popup_title.setText(R.string.confirm_add_popup)
-            }
-            product_details_button.setOnClickListener { bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED) }
-            popup_cancel.setOnClickListener { bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN) }
-            popup_confirm.setOnClickListener {
-                val product = Product(
-                    Integer.parseInt(catalogProduct.ref), catalogProduct.title,
-                    catalogProduct.description, catalogProduct.thumbnail, catalogProduct.price!!
-                )
-                if (!isAddedToBaset) {
-                    presenter.addProduct(product)
-                    preferences.putBoolean(catalogProduct.ref, true)
-                } else {
-                    presenter.removeProduct(product)
-                    preferences.putBoolean(catalogProduct.ref, false)
-                }
-                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN)
-            }
+            fillProductDetails()
+            manageBottomsheet()
         }
     }
 
-
-    override fun showProgress() {
-        product_details_progress_bar.visibility = View.VISIBLE
+    private fun initBottomSheet() {
+        bottomSheetBehavior = BottomSheetBehavior.from(popup_bottom_sheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
     }
 
-    override fun hideProgress() {
-        product_details_progress_bar.visibility = View.GONE
+    private fun injectDependencies() {
+        UABigBurgerKotlinApp.uaBigBurgerAppComponent.inject(this)
     }
 
-    override fun onProductAdded() {
-        product_details_button.setText(R.string.remove_from_basket)
-        popup_title.setText(R.string.confirm_delete_popup)
-        isAddedToBaset = true
-        Toast.makeText(applicationContext, R.string.adding_to_basket_success, Toast.LENGTH_SHORT).show()
+    private fun fillProductDetails() {
+        catalogProduct =
+            Gson().fromJson(intent?.getExtras()?.getString(catalogProductKey), CatalogProductModel::class.java)
+        isAddedToBaset = preferences.getBoolean(catalogProduct.ref, false)
+        title = catalogProduct.title
+        product_details_price.text = String.format(getResources().getString(R.string.format), catalogProduct.price)
+        product_details_description.text = catalogProduct.description
+        Glide.with(applicationContext)
+            .load(catalogProduct.thumbnail)
+            .apply(
+                RequestOptions()
+                    .placeholder(R.drawable.no_image)
+                    .error(R.drawable.no_image)
+            )
+            .into(product_details_thumbnail)
+    }
+
+    private fun manageBottomsheet() {
+        if (isAddedToBaset) {
+            product_details_button.setText(R.string.remove_from_basket)
+            popup_title.setText(R.string.confirm_delete_popup)
+        } else {
+            product_details_button.setText(R.string.add_to_basket)
+            popup_title.setText(R.string.confirm_add_popup)
+        }
+        product_details_button.setOnClickListener { bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED) }
+        popup_cancel.setOnClickListener { bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN) }
+        popup_confirm.setOnClickListener {
+            val product = Product(
+                Integer.parseInt(catalogProduct.ref), catalogProduct.title,
+                catalogProduct.description, catalogProduct.thumbnail, catalogProduct.price!!
+            )
+            if (!isAddedToBaset) {
+                presenter.addProduct(product)
+                preferences.putBoolean(catalogProduct.ref, true)
+            } else {
+                presenter.removeProduct(product)
+                preferences.putBoolean(catalogProduct.ref, false)
+            }
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN)
+        }
     }
 
     override fun onDestroy(disposable: Disposable) {
         disposable.dispose()
     }
 
-    override fun onRemoveSuccess() {
-        product_details_button.setText(R.string.add_to_basket)
-        popup_title.setText(R.string.confirm_add_popup)
-        isAddedToBaset = false
-        Toast.makeText(applicationContext, "Product successfully removed!", Toast.LENGTH_SHORT).show()
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        menu.findItem(R.id.basket).isVisible = false
+        return true
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -124,13 +126,27 @@ class ProductDetailsActivity : AppCompatActivity(), ProductDetailsView {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        menu.findItem(R.id.basket).isVisible = false
-        return true
+    override fun showProgress() {
+        product_details_progress_bar.visibility = View.VISIBLE
     }
 
-    companion object {
-        const val catalogProductKey = "CATALOG_PRODUCT"
+    override fun hideProgress() {
+        product_details_progress_bar.visibility = View.GONE
     }
+
+    override fun onProductAdded() {
+        product_details_button.setText(R.string.remove_from_basket)
+        popup_title.setText(R.string.confirm_delete_popup)
+        isAddedToBaset = true
+        Toast.makeText(applicationContext, R.string.adding_to_basket_success, Toast.LENGTH_SHORT).show()
+    }
+
+
+    override fun onRemoveSuccess() {
+        product_details_button.setText(R.string.add_to_basket)
+        popup_title.setText(R.string.confirm_add_popup)
+        isAddedToBaset = false
+        Toast.makeText(applicationContext, "Product successfully removed!", Toast.LENGTH_SHORT).show()
+    }
+
 }
